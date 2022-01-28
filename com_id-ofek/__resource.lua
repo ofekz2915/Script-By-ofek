c0c412071ac13979320e105f9a86768a0f3cd21c
ofek#4859 <79029993+ofekz2915@users.noreply.github.com>
2022-01-28 10:59:57 +0200

    Delete com_id-ofek directory

--- com_id-ofek/__resource.lua
+++
@@ -1,6 +0,0 @@
---[[
-      script By ofek1s#4859
-    Description: /id  
-]]
-
-client_script "client.lua"
--- com_id-ofek/client.lua
+++
@@ -1,9 +0,0 @@
-RegisterCommand('id', 
-
-function()
-    msg("^9" .. GetPlayerName(PlayerId()) .. " ")
-end, false)
-
-function msg(text)
-    TriggerEvent("chatMessage",  "ID: " .. GetPlayerServerId(PlayerId()) .. "", {255, 0, 0}, text)
-end
